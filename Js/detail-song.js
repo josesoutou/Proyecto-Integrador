ef97b5900f6d135d999f3be56a689b7161010865
Foncho81z <alfonsoagote@gmail.com>
2021-06-17 15:48:54 -0300

    probando el reproductor de música en detail-songs

--- Js/detail-song.js
+++ Js/detail-song.js
@@ -1,20 +1,15 @@
 window.addEventListener("load", function() {
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     let apiCanciones = `${proxy}https://api.deezer.com/playlist/9165177722`;
-    
-    
     fetch(apiCanciones)
     .then(function (response) {
         return response.json();
     })
     .then(function (datos) {
         console.log(datos);
-        let tituloCancion1 = document.querySelectorAll('.fa-play'); 
-        console.log(tituloCancion1)   
-        for(let index = 0; index < tituloCancion1.length; index++){
-            tituloCancion1[index].addEventListener("click", function () {
-                document.body.innerHTML += `<iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/playlist/9165177722" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`
-        })
+        let insertarReproductorDeAudio = document.querySelectorAll('#estrellitafachera');
+        for(let index = 0; index < insertarReproductorDeAudio.length; index++){
+            insertarReproductorDeAudio[index].innerHTML = `<iframe src="${datos.tracks.data[index].preview}" width="100px" height="70" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`;
         }    
     })
     .catch(function (error) {
